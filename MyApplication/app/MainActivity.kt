19f54289bbebdbad90b58c449537e4900b9f50ec
GeoffNderitu <85866046+GeoffNderitu@users.noreply.github.com>
2023-06-13 09:22:46 +0300

    Add files via upload

---
+++ MyApplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set up button click listener
+        startButton.setOnClickListener {
+            // Perform action when start button is clicked
+        }
+    }
+}
